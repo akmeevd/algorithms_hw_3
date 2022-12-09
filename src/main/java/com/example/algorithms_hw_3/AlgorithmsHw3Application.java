10c72b48ff7bea63a7a50fed25446064a8ae6b66
Dima <akmeev2011@yandex.ru>
2022-12-09 17:12:15 +0300

    Initial commit

---
+++ src/main/java/com/example/algorithms_hw_3/AlgorithmsHw3Application.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_hw_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsHw3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmsHw3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/algorithms_hw_3/AlgorithmsHw3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.algorithms_hw_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsHw3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
